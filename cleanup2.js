c90c9e601c37ef80a34bf3b4dcffe9ccc76e6b2b
Kathir R <kathir.3447@gmail.com>
2026-09-22 18:57:23 +0530

    chore: remove helper script

--- cleanup2.js
+++ cleanup2.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 
-try { fs.unlinkSync('cleanup.js'); } catch(e) {}
+try { fs.unlinkSync('final-commit.js'); } catch(e) {}
 
 execSync('git add -A', { cwd: process.cwd() });
-execSync('git commit -m "chore: final state - build verified, page.tsx fixed" --no-verify', { cwd: process.cwd() });
+execSync('git commit -m "chore: remove helper script" --no-verify', { cwd: process.cwd() });
 execSync('git push', { cwd: process.cwd() });
 console.log('done');
