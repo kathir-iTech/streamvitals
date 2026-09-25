7793dcfb16ab6c39279ab80ec73f75e9d5504aad
Kathir R <kathir.3447@gmail.com>
2026-09-26 00:37:48 +0530

    fix: clean package-lock.json, remove undefined version entries

--- deploy.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-try { execSync('git add -A'); } catch(e) {}
-try { execSync('git commit -m "remove deploy.js helper"'); } catch(e) {}
-try { execSync('git push'); } catch(e) { console.log(e.message.substring(0,200)); }
