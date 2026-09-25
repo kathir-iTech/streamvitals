176fe30d59462c38219490109c583bb8f60944a2
Kathir R <kathir.3447@gmail.com>
2026-09-25 17:57:16 +0530

    remove commit.js helper

--- commit.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-execSync('git add -A', { stdio: 'inherit' });
-execSync('git commit -m "revert vercel json finalize Field Companion"', { stdio: 'inherit' });
-execSync('git push', { stdio: 'inherit' });
