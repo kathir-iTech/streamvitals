efaea343b93c52173e15ce6516f6659cb59c3626
Kathir R <kathir.3447@gmail.com>
2026-09-26 00:50:56 +0530

    remove deploy.js

--- deploy.js
+++ deploy.js
@@ -1,4 +1,2 @@
 const { execSync } = require('child_process');
-try { execSync('git add -A'); } catch(e) {}
-try { execSync('git commit -m "remove deploy.js"'); } catch(e) {}
-try { execSync('git push'); } catch(e) { console.log(e.message.substring(0,200)); }
+try { execSync('git add -A'); execSync('git commit -m "remove deploy.js"'); execSync('git push'); } catch(e) { console.log(e.message.substring(0,200)); }
