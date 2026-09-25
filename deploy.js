21f547f2a3414f348c365f7f6a399ff18f298db8
Kathir R <kathir.3447@gmail.com>
2026-09-26 00:49:25 +0530

    remove helper scripts

--- deploy.js
+++ deploy.js
@@ -1,4 +1,4 @@
 const { execSync } = require('child_process');
 try { execSync('git add -A'); } catch(e) {}
-try { execSync('git commit -m "fix: import actual indicator data from JSON, was empty array"'); } catch(e) {}
+try { execSync('git commit -m "remove helper scripts"'); } catch(e) {}
 try { execSync('git push'); } catch(e) { console.log(e.message.substring(0,200)); }
