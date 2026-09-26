04ae039f0057c795860bc5f549513f980d726593
kathir-itech <kathir-itech@example.com>
2026-09-26 09:39:18 +0530

    chore: remove deploy helper

--- deploy.js
+++ deploy.js
@@ -1,2 +1,2 @@
 const { execSync } = require('child_process');
-try { execSync('git add -A'); execSync('git commit -m "remove deploy.js"'); execSync('git push'); } catch(e) {}
+try { execSync('git add -A'); execSync('git commit -m "chore: remove deploy helper"'); execSync('git push'); } catch(e) { console.log(e.message.substring(0,200)); }
